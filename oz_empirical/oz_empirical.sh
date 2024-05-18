47c572d2a7ef7902194d462d1ffe20903b4fe051
Tomasz Woźniak <wozniak.tom@pm.me>
2024-05-18 18:50:38 +1000

    Create oz_empirical.sh

---
+++ oz_empirical/oz_empirical.sh
@@ -0,0 +1,46 @@
+# setup the password as an environmental variable
+source ~/.bash-profile
+
+# just to check the progress of simulations on spartan
+sshpass -e ssh twozniak@spartan.hpc.unimelb.edu.au
+squeue -u twozniak
+exit
+
+
+# Upload files
+sshpass -e scp oz_empirical/bsvars_data.rda twozniak@spartan.hpc.unimelb.edu.au:/data/projects/punim0093/bsvars_mcxs/
+sshpass -e scp oz_empirical/*.R twozniak@spartan.hpc.unimelb.edu.au:/data/projects/punim0093/bsvars_mcxs/
+sshpass -e scp oz_empirical/*.slurm twozniak@spartan.hpc.unimelb.edu.au:/data/projects/punim0093/bsvars_mcxs/
+
+# Download and upload RData files
+scp twozniak@spartan.hpc.unimelb.edu.au:/data/projects/punim0093/bsvars_mcxs/*rw.rda oz_empirical/
+
+# working with svar_betel on spartan
+#################################################
+sshpass -e ssh twozniak@spartan.hpc.unimelb.edu.au
+cd /data/projects/punim0093/bsvars_mcxs/
+
+sbatch bsvars_nr.slurm 
+squeue -u twozniak
+
+rm *.out
+cat *63.out
+cat *MR.slurm
+tail -200 source_code/bsvar_sv.cpp
+ls
+ls results/
+ls source_code/
+exit
+#
+
+# install bsvars package on spartan
+#################################################
+sshpass -e ssh twozniak@spartan.hpc.unimelb.edu.au
+sinteractive
+module load R/4.2.1
+R
+devtools::install_git("https://github.com/bsvars/bsvars.git")
+bsvars::bsvar
+q("no")
+exit
+
